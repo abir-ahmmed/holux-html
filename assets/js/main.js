95a0e1164ff11f2d5db76a606b50bf07b131bce6
abir-ahmmed <abirahmmedrana@outlook.com>
2022-12-06 16:06:57 +0600

    popular section running

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,13 @@
 
 /*========================================  ========================================*/
 /*======================================== CHANGE BACKGROUND HEADER ========================================*/
-function scrollHeader(){
+const scrollHeader = () =>{
     const header = document.getElementById('header')
     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    if(this.scroll => 50) header.classList.add('scroll-header');
-        else header.classList.remove('scroll-header')
+    this.scrollY >= 50 ? header.classList.add('scroll-header')
+        : header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 /*========================================  ========================================*/
 /*========================================  ========================================*/
-    
+
