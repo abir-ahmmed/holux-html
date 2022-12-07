1534737977c6de52386f47d31e7018f0e9ef4996
abir-ahmmed <abirahmmedrana@outlook.com>
2022-12-07 18:58:42 +0600

    now contact

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,5 +22,34 @@ var swiperPopular = new Swiper(".popular_container", {
       prevEl: ".swiper-button-prev",
     },
   });
+/*======================================== Value JS ========================================*/
+const accordionItems = document.querySelectorAll ('.value_accordion-item')
+
+accordionItems.forEach((item) =>{
+  const accordionHeader = item.querySelector('.value_accordion-header')
+
+  accordionHeader.addEventListener('click', () => {
+    const openItem = document.querySelector('.accordion-open')
+    
+    toggleItem(item)
+
+    if(openItem && openItem!== item){
+      toggleItem(openItem)
+    }
+  })
+})
+
+const toggleItem = (item) =>{
+  const accordionContent = item.querySelector('.value_accordion-content')
+
+  if(item.classList.contains('accordion-open')){
+    accordionContent.removeAttribute('style')
+    item.classList.remove('accordion-open')
+  }else{
+      accordionContent.style.height = accordionContent.scrollHeight + 'px'
+      item.classList.add('accordion-open')
+
+  }
+}
 /*========================================  ========================================*/
 
